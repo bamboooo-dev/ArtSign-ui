7cbe60426bc46724c74b65a7ac9bc79c88931b41
Yuya Sumie <monamonacan0926@gmail.com>
2021-11-10 14:27:27 +0900

    URQL Get Start

--- App.js
+++ App.js
@@ -9,11 +9,24 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import Config from "react-native-config";
+import { createClient, Provider } from 'urql';
 import CommunityScreen from './src/screens/Community';
 import HomeScreen from './src/screens/Home';
 import TreasureScreen from './src/screens/Treasure';
+import UrqlTestScreen from './src/screens/UrqlTest';
 
 const Tab = createBottomTabNavigator();
+
+const client = createClient({
+  url: 'https://api.github.com/graphql',
+  fetchOptions: () => {
+    const token = Config.REACT_APP_GITHUB_AUTH_TOKEN;
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+    };
+  },
+});
 
 const App = () => {
   return (
@@ -31,9 +44,21 @@ const App = () => {
           name="Treasure"
           component={TreasureScreen}
         />
+        <Tab.Screen
+          name="UrqlTest"
+          component={UrqlTestScreen}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+const AppRoot = () => {
+  return (
+    <Provider value={client}>
+      <App />
+    </Provider>
+  );
+}
+
+export default AppRoot;
