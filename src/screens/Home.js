8f5e74892a2565345267774296ea1ce9919d33f3
Takoyarou <kou1105dane@gmail.com>
2021-11-14 04:57:10 +0900

    タブ切り替え処理途中

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,15 +3,29 @@ import {
   StyleSheet,
   SafeAreaView,
   StatusBar, Text, View,
-  Button,
+  ScrollView,
   TouchableOpacity,
 } from 'react-native';
 
 const HomeScreen = () => {
+  i = -1;
+  tabs = ["デザイン", "イラスト", "油画", "日本画", "写真", "建築","彫刻"];
+  nums = [];
+  list = tabs.map(() => {
+    i+=1;
+    nums[i] = Math.pow(i, 2);
+    return (
+      <TouchableOpacity onPress={() => {this.setState({selectedNum: i,});}}>
+        <Text style={detectTab((nums[i] >> this.state.selectedNum) & 0b01)}>
+          {tabs[i]}
+        </Text>
+      </TouchableOpacity>
+    );
+  });
   /*constructor(props){
     super(props);
-    this.state = {tab: "hot"};
-  }
+    this.state = {tab: "hot", selectedNum: 0};
+  }*/
   switchTab = () => {
     if(this.state.tab == "hot"){
       this.setState({tab: "recommend"});
@@ -20,31 +34,91 @@ const HomeScreen = () => {
       this.setState({tab: "hot"});
     }
   };
-  detectTabState = () => {
-    if(this.state.tab == "hot"){
 
+  detectCurrentTab = ()=> {
+    if(this.state.tab == "hot"){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  detectIsHot = (b) => {
+    if(b){
+      return selectedButton;
     }
     else{
-      
+      return notSelectedButton;
     }
-  };*/
+  };
+  detectTab = (b) => {
+    if(b){
+      return styles.selectedButton;
+    }else{
+      return styles.notSelectedButton;
+    }
+  }
   return (
     <SafeAreaView>
       <StatusBar />
       <View style={styles.switchingTab}>
-        <TouchableOpacity>
-          <Text style={styles.selectedButton}>
+        <TouchableOpacity onPress={switchTab()}>
+          <Text style={detectIsHot(detectCurrentTab())}>
             急上昇
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.notSelectedButton}>
+        <TouchableOpacity onPress={switchTab()}>
+          <Text style={detectIsHot(!detectCurrentTab())}>
             あなたへのおすすめ
           </Text>
         </TouchableOpacity>
       </View>
       <View style={styles.switchingTab}>
-
+        <ScrollView horizontal={true}>
+          /*<TouchableOpacity>
+            <Text style={styles.selectedButton}>
+              デザイン
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.selectedButton}>
+              イラスト
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.selectedButton}>
+              油画
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.selectedButton}>
+              日本画
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.selectedButton}>
+              写真
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.selectedButton}>
+              建築
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.selectedButton}>
+              彫刻
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>*/
+        <ScrollView horizontal={true}>
+          <li>
+            {list}
+          </li>
+        </ScrollView>
+        <ScrollView>
+          
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
